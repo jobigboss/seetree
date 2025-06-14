b524f018e2a1d395086f1d1b46de4716aff8d39a
Joe Bigboss <josursit@gmail.com>
2025-06-14 12:46:47 +0700

    up

--- src/app/api/register/[userId]/route.js
+++ src/app/api/register/[userId]/route.js
@@ -6,9 +6,9 @@ import { NextResponse } from 'next/server'
 export async function GET(req, { params }) {
   await connectMongoDB();
   const { userId } = params;
-  console.log("API GET userId =", userId); // log นี้ต้องแสดง userId ที่รับเข้ามา
+  console.log("API GET userId", userId);
   const user = await Register.findOne({ regLineID: userId });
-  console.log("user found =", user);
+  console.log("API found user", user);
   if (!user) {
     return NextResponse.json({ message: "Not found" }, { status: 404 });
   }
